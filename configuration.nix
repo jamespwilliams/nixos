2e6fea914c226a616d8178f19941d7a0a3c70d47
James Williams <jamesphwilliams@gmail.com>
2020-12-13 16:06:47 +0000

    Add some packages

--- configuration.nix
+++ configuration.nix
@@ -63,8 +63,10 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    gnumake
+    gcc
+    xorg.libX11
     wget
-    vim
     tmux
     chromium
     git
@@ -74,6 +76,10 @@
     fzf
     libnotify
     thunderbird
+    dmenu
+    bc
+    xautolock
+    dunst
   ];
 
   hardware.bluetooth.enable = true;
