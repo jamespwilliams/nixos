8e5b0eb01a17227ccef840530184be81c6891184
James Williams <jamesphwilliams@gmail.com>
2021-01-01 13:39:54 +0000

    Add yubikey gpg-agent configuration

--- configuration.nix
+++ configuration.nix
@@ -190,6 +190,26 @@
     };
   };
 
+  # YubiKey/gpg setup:
+  services.udev.packages = [ pkgs.yubikey-personalization ];
+
+  services.pcscd.enable = true;
+  environment.shellInit = ''
+    export GPG_TTY="$(tty)"
+    gpg-connect-agent /bye
+    export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+  '';
+
+  programs = {
+    ssh.startAgent = false;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+  };
+
+  # /end YubiKey/gpg setup
+
   powerManagement.powerDownCommands = "
     systemctl stop openvpn-work-vpn
   ";
